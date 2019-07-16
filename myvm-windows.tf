d74db9d931812226f2e954f416261511ad8c9c0d
Nico Vibert <nvibert@vmware.com>
2019-07-16 15:48:23 +0100

    Update and rename myvm.tf to myvm-windows.tf

--- myvm-windows.tf
+++ myvm-windows.tf
@@ -23,7 +23,7 @@ data "vsphere_network" "network" {
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "Windows-Template-Update-Tools-Running"
+  name          = "Windows-Template"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
